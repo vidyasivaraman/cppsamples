29747e0f759597b232a8df50bb55117fd2b199d2
Vidya Sivaraman <vidyasivaraman@Balajis-MacBook-Pro.local>
2017-01-26 21:07:52 -0800

    Initial Commit

---
+++ cppSamples/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cppSamples
+//
+//  Created by Vidya Sivaraman on 1/26/17.
+//  Copyright © 2017 Vidya Sivaraman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
